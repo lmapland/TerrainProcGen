ba20ca6d329c67735181741ea56eed5dc6f77e06
Leslie <leslie.apland@gmail.com>
2024-02-10 15:58:21 -0800

    Added Foliage generation

--- Source/TerrainProcGen/Private/WorldGenerator.cpp
+++ Source/TerrainProcGen/Private/WorldGenerator.cpp
@@ -2,8 +2,10 @@
 
 
 #include "WorldGenerator.h"
-#include "ProceduralMeshComponent.h"
 #include "KismetProceduralMeshLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+#include "FoliageType_InstancedStaticMesh.h"
 
 //#include "Debug/DebugDrawComponent.h"
 
@@ -12,13 +14,25 @@ AWorldGenerator::AWorldGenerator()
 	PrimaryActorTick.bCanEverTick = false;
 
 	TerrainMesh = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("TerrainMesh"));
+	TerrainMesh->bUseAsyncCooking = true;
 	RootComponent = TerrainMesh;
+
 }
 
 void AWorldGenerator::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	TileDiscardDistance = CellSize * (NumberSectionsX + NumberSectionsY) / 2 * (XVertexCount + YVertexCount) / 2;
+
+	//UE_LOG(LogTemp, Warning, TEXT("TileDiscardDistance: %f"), TileDiscardDistance);
+
+	if (bRandomizeLayout)
+	{
+		PerlinOffset = FVector2D(FMath::FRandRange(-5000000.f, 5000000.f), FMath::FRandRange(-5000000.f, 5000000.f));
+		//AmplitudeLarge = AmplitudeLarge * FMath::FRandRange(.5f, 2.f);
+		//AmplitudeMedium = AmplitudeMedium * FMath::FRandRange(.5f, 2.f);
+	}
 }
 
 void AWorldGenerator::GenerateTerrain(const int InSectionIndexX, const int InSectionIndexY)
@@ -31,7 +45,6 @@ void AWorldGenerator::GenerateTerrain(const int InSectionIndexX, const int InSec
 	FVector Vertex;
 	TArray<FVector2D> UVs;
 	FVector2D UV;
-	TArray<int> Triangles;
 
 	for (int y = -1; y <= YVertexCount; y++)
 	{
@@ -51,56 +64,108 @@ void AWorldGenerator::GenerateTerrain(const int InSectionIndexX, const int InSec
 		}
 	}
 	//UE_LOG(LogTemp, Warning, TEXT("GenerateTerrain(): after first for loop"));
-
-	for (int y = 0; y <= YVertexCount; y++)
+	if (Triangles.Num() == 0)
 	{
-		for (int x = 0; x <= XVertexCount; x++)
+		for (int y = 0; y <= YVertexCount; y++)
 		{
-			int XYVertexCount = x + (y * (XVertexCount + 2));
-			Triangles.Add(XYVertexCount);
-			//UE_LOG(LogTemp, Warning, TEXT("%i, %i: %i"), y, x, XYVertexCount);
-			Triangles.Add(XYVertexCount + (XVertexCount + 2));
-			//UE_LOG(LogTemp, Warning, TEXT("%i, %i: %i"), y, x, XYVertexCount + (XVertexCount + 2));
-			Triangles.Add(XYVertexCount + 1);
-			//UE_LOG(LogTemp, Warning, TEXT("%i, %i: %i"), y, x, XYVertexCount + 1);
+			for (int x = 0; x <= XVertexCount; x++)
+			{
+				int XYVertexCount = x + (y * (XVertexCount + 2));
+				Triangles.Add(XYVertexCount);
+				//UE_LOG(LogTemp, Warning, TEXT("%i, %i: %i"), y, x, XYVertexCount);
+				Triangles.Add(XYVertexCount + (XVertexCount + 2));
+				//UE_LOG(LogTemp, Warning, TEXT("%i, %i: %i"), y, x, XYVertexCount + (XVertexCount + 2));
+				Triangles.Add(XYVertexCount + 1);
+				//UE_LOG(LogTemp, Warning, TEXT("%i, %i: %i"), y, x, XYVertexCount + 1);
 
-			Triangles.Add(XYVertexCount + (XVertexCount + 2));
-			//UE_LOG(LogTemp, Warning, TEXT("%i, %i: %i"), y, x, XYVertexCount + (XVertexCount + 2) );
-			Triangles.Add(XYVertexCount + (XVertexCount + 2) + 1);
-			//UE_LOG(LogTemp, Warning, TEXT("%i, %i: %i"), y, x, XYVertexCount + (XVertexCount + 2) + 1);
-			Triangles.Add(XYVertexCount + 1);
-			//UE_LOG(LogTemp, Warning, TEXT("%i, %i: %i"), y, x, XYVertexCount + 1);
+				Triangles.Add(XYVertexCount + (XVertexCount + 2));
+				//UE_LOG(LogTemp, Warning, TEXT("%i, %i: %i"), y, x, XYVertexCount + (XVertexCount + 2) );
+				Triangles.Add(XYVertexCount + (XVertexCount + 2) + 1);
+				//UE_LOG(LogTemp, Warning, TEXT("%i, %i: %i"), y, x, XYVertexCount + (XVertexCount + 2) + 1);
+				Triangles.Add(XYVertexCount + 1);
+				//UE_LOG(LogTemp, Warning, TEXT("%i, %i: %i"), y, x, XYVertexCount + 1);
+			}
 		}
 	}
 
-	//UE_LOG(LogTemp, Warning, TEXT("GenerateTerrain(): after second for loop"));
-
-	TArray<FVector> SubNormals;
-	TArray<FProcMeshTangent> SubTangents;
-	TArray<FVector2D> SubUVs;
-	TArray<int> SubTriangles;
-	TArray<FVector> SubVertices;
 	GetNormalsTangents(Vertices, Triangles, UVs, SubNormals, SubTangents, SubVertices, SubTriangles, SubUVs);
-	//UE_LOG(LogTemp, Warning, TEXT("%i, %i, %i, %i, %i"), SubNormals.Num(), SubTangents.Num(), SubUVs.Num(), SubTriangles.Num(), SubVertices.Num());
-	TerrainMesh->CreateMeshSection_LinearColor(MeshSectionIndex, SubVertices, SubTriangles, SubNormals, SubUVs, TArray<FLinearColor>(), SubTangents, true);
-	if (TerrainMaterial)
+	bTileDataReady = true;
+}
+
+int AWorldGenerator::DrawTile()
+{
+	//UE_LOG(LogTemp, Warning, TEXT("DrawTile()"));
+	bTileDataReady = false;
+
+	int DrawnMeshSection;
+
+	int FurthestTileIndex = GetFurthestUpdatableTileIndex();
+	UE_LOG(LogTemp, Warning, TEXT("FurthestTileIndex: %i"), FurthestTileIndex);
+
+	if (FurthestTileIndex > -1)
 	{
-		TerrainMesh->SetMaterial(MeshSectionIndex, TerrainMaterial);
+		// Found a tile that needs to be replaced
+		TArray<int> ValueArray;
+		TArray<FIntPoint> KeyArray;
+		QueuedTiles.GenerateKeyArray(KeyArray);
+		QueuedTiles.GenerateValueArray(ValueArray);
+
+		int ReplaceableMeshSection = ValueArray[FurthestTileIndex];
+		DrawnMeshSection = ReplaceableMeshSection;
+		RemoveFoliageTile(ReplaceableMeshSection);
+
+		//TerrainMesh->UpdateMeshSection(ReplaceableMeshSection, SubVertices, SubNormals, SubUVs, TArray<FColor>(), SubTangents);
+		TerrainMesh->ClearMeshSection(ReplaceableMeshSection);
+		TerrainMesh->CreateMeshSection_LinearColor(ReplaceableMeshSection, SubVertices, SubTriangles, SubNormals, SubUVs, TArray<FLinearColor>(), SubTangents, true);
+
+		QueuedTiles.Add(FIntPoint(SectionIndexX, SectionIndexY), ReplaceableMeshSection);
+		QueuedTiles.Remove(KeyArray[FurthestTileIndex]);
 	}
-	MeshSectionIndex++;
+	else
+	{
+		TerrainMesh->CreateMeshSection_LinearColor(MeshSectionIndex, SubVertices, SubTriangles, SubNormals, SubUVs, TArray<FLinearColor>(), SubTangents, true);
+		if (TerrainMaterial)
+		{
+			TerrainMesh->SetMaterial(MeshSectionIndex, TerrainMaterial);
+		}
+		DrawnMeshSection = MeshSectionIndex;
+		MeshSectionIndex++;
+	}
+
+	SubVertices.Empty();
+	SubNormals.Empty();
+	SubTangents.Empty();
+	SubUVs.Empty();
+
+	return DrawnMeshSection;
+}
+
+void AWorldGenerator::GenerateTerrainAsync(const int InSectionIndexX, const int InSectionIndexY)
+{
+	bGeneratorBusy = true;
+
+	SectionIndexX = InSectionIndexX;
+	SectionIndexY = InSectionIndexY;
+	QueuedTiles.Add(FIntPoint(SectionIndexX, SectionIndexY), MeshSectionIndex);
+
+	AsyncTask(ENamedThreads::AnyBackgroundThreadNormalTask, [&]()
+		{
+			auto WorldGenTask = new FAsyncTask<FAsyncWorldGenerator>(this);
+			WorldGenTask->StartBackgroundTask();
+			WorldGenTask->EnsureCompletion();
+			delete WorldGenTask;
+		}
+	);
 }
 
 int AWorldGenerator::GenHeight(FVector2D Location)
 {
-	return GenerateHeightLayer(Location, ScaleLarge, AmplitudeLarge, FVector2D(.1f)); // + GenerateHeightLayer(Location, ScaleMedium, AmplitudeMedium, FVector2D(.2f)) + GenerateHeightLayer(Location, ScaleSmall, AmplitudeSmall, FVector2D(.4f));
+	return GenerateHeightLayer(Location, 1 / ScaleLarge, AmplitudeLarge, FVector2D(.1f) + PerlinOffset) + GenerateHeightLayer(Location, 1 / ScaleMedium, AmplitudeMedium, FVector2D(.2f) + PerlinOffset);
 }
 
 int AWorldGenerator::GenerateHeightLayer(const FVector2D Location, const float InScale, const float InAmplitude, const FVector2D Offset)
 {
 	float Result2 = FMath::PerlinNoise2D(Location * InScale + FVector2D(.1f, .1f) + Offset) * InAmplitude;
-	FVector2D Perlin1 = Location * InScale + FVector2D(.1f, .1f);
-	FVector2D Perlin2 = Perlin1 + Offset;
-	float Result = FMath::PerlinNoise2D(Perlin2) * InAmplitude;
 	return Result2;
 
 	//UE_LOG(LogTemp, Warning, TEXT("PerlinNoise2D: InValue: (%f, %f); Perlin Result: %f"), Perlin2.X, Perlin2.Y, Result);
@@ -120,7 +185,6 @@ int AWorldGenerator::GenerateHeightLayer(const FVector2D Location, const float I
 void AWorldGenerator::GetNormalsTangents(TArray<FVector> InVertices, TArray<int> InTriangles, TArray<FVector2D> InUVs, TArray<FVector>& OutNormals, TArray<FProcMeshTangent>& OutTangents, TArray<FVector>& OutVertices, TArray<int>& OutTriangles, TArray<FVector2D>& OutUVs)
 {
 	//UE_LOG(LogTemp, Warning, TEXT("In GetNormalsTangents()"));
-	TArray<int> Triangles;
 	TArray<FVector> Normals;
 	TArray<FProcMeshTangent> Tangents;
 
@@ -143,22 +207,169 @@ void AWorldGenerator::GetNormalsTangents(TArray<FVector> InVertices, TArray<int>
 		}
 	}
 
-	for (int y = 0; y < YVertexCount - 1; y++)
+	if (OutTriangles.Num() == 0)
 	{
-		for (int x = 0; x < XVertexCount - 1; x++)
+		for (int y = 0; y < YVertexCount - 1; y++)
 		{
-			int XYVertexCount = y * XVertexCount + x;
-			OutTriangles.Add(XYVertexCount);
-			OutTriangles.Add(XYVertexCount + XVertexCount);
-			OutTriangles.Add(XYVertexCount + 1);
+			for (int x = 0; x < XVertexCount - 1; x++)
+			{
+				int XYVertexCount = y * XVertexCount + x;
+				OutTriangles.Add(XYVertexCount);
+				OutTriangles.Add(XYVertexCount + XVertexCount);
+				OutTriangles.Add(XYVertexCount + 1);
 
-			OutTriangles.Add(XYVertexCount + XVertexCount);
-			OutTriangles.Add(XYVertexCount + XVertexCount + 1);
-			OutTriangles.Add(XYVertexCount + 1);
+				OutTriangles.Add(XYVertexCount + XVertexCount);
+				OutTriangles.Add(XYVertexCount + XVertexCount + 1);
+				OutTriangles.Add(XYVertexCount + 1);
+			}
 		}
 	}
 
 	//UE_LOG(LogTemp, Warning, TEXT("%i, %i, %i, %i, %i"), OutVertices.Num(), OutTriangles.Num(), OutNormals.Num(), OutUVs.Num(), OutTangents.Num());
+}
+
+FVector AWorldGenerator::GetPlayerLocation()
+{
+	ACharacter* Player = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+
+	return Player ? Player->GetActorLocation() : FVector(0.f);
+}
+
+FVector2D AWorldGenerator::GetTileLocation(FIntPoint TileCoordinate)
+{
+	return FVector2D(TileCoordinate * FIntPoint(XVertexCount - 1, YVertexCount - 1) * CellSize) + FVector2D(XVertexCount - 1, YVertexCount - 1) * CellSize / 2;
+}
+
+FIntPoint AWorldGenerator::GetClosestQueuedTile()
+{
+	if (QueuedTiles.Num() == 0) return FIntPoint(0.f);
+
+	float ClosestDistance = TNumericLimits<float>::Max();
+	FIntPoint ClosestTile;
+
+	for (const auto& Tile : QueuedTiles)
+	{
+		const FIntPoint& Key = Tile.Key;
+		int Value = Tile.Value;
+		if (Value == -1) // Find only unrendered tiles
+		{
+			FVector2D TileLocation = GetTileLocation(Key);
+			FVector PlayerLocation = GetPlayerLocation();
+			float Distance = FVector2D::Distance(TileLocation, FVector2D(PlayerLocation));
+
+			if (Distance < ClosestDistance)
+			{
+				ClosestDistance = Distance;
+				ClosestTile = Key;
+			}
+		}
+	}
+	return ClosestTile;
+}
+
+int AWorldGenerator::GetFurthestUpdatableTileIndex()
+{
+	if (QueuedTiles.Num() == 0) return -1;
+
+	float FurthestDistance = -1.f;
+	int FurthestTileIndex = -1;
+	int CurrentIndex = 0;
+
+	for (const auto& Tile : QueuedTiles)
+	{
+		const FIntPoint& Key = Tile.Key;
+		int Value = Tile.Value;
+		if (Value != -1) // Find only rendered tiles
+		{
+			FVector2D TileLocation = GetTileLocation(Key);
+			FVector PlayerLocation = GetPlayerLocation();
+			float Distance = FVector2D::Distance(TileLocation, FVector2D(PlayerLocation));
+
+			if (Distance > FurthestDistance && Distance > TileDiscardDistance)
+			{
+				FurthestDistance = Distance;
+				FurthestTileIndex = CurrentIndex;
+			}
+		}
+		CurrentIndex++;
+	}
+	return FurthestTileIndex;
+}
+
+void AWorldGenerator::AddFoliageInstances(const FVector InLocation)
+{
+	for (int FoliageTypeIndex = 0; FoliageTypeIndex < FoliageTypes.Num(); FoliageTypeIndex++)
+	{
+		UFoliageType_InstancedStaticMesh* FoliageType = FoliageTypes[FoliageTypeIndex];
+		if (!FoliageType)
+		{
+			continue;
+		}
+
+		if (InLocation.Z < FoliageType->Height.Min || InLocation.Z > FoliageType->Height.Max)
+		{
+			continue;
+		}
+
+		if (FoliageType->InitialSeedDensity < FoliageSeed.FRandRange(0.f, 10.f))
+		{
+			continue;
+		}
+
+		UInstancedStaticMeshComponent* FoliageInstance = FoliageComponents[FoliageTypeIndex];
+		SpawnFoliageCluster(FoliageType, FoliageInstance, InLocation);
+	}
+}
+
+void AWorldGenerator::SpawnFoliageCluster(UFoliageType_InstancedStaticMesh* FoliageType, UInstancedStaticMeshComponent* FoliageIsmComponent, const FVector ClusterLocation)
+{
+	int MaxSteps = FoliageSeed.RandRange(0, FoliageType->NumSteps);
+	int MaxSeeds = FoliageSeed.RandRange(0, FoliageType->SeedsPerStep);
+	FVector ClusterBase = ClusterLocation;
+
+	for (int i = 0; i < MaxSteps; i++)
+	{
+		ClusterBase += FoliageSeed.GetUnitVector() * FoliageType->AverageSpreadDistance;
+		int InstancesPerStep = 0;
+
+		for (int l = 0; l < MaxSeeds; l++)
+		{
+			FVector InstanceLocation = ClusterBase + FoliageSeed.GetUnitVector() * FoliageType->SpreadVariance;
+
+			FHitResult HitResult;
+			FCollisionQueryParams CollisionParams;
+			bool bHit = GetWorld()->LineTraceSingleByChannel(HitResult, InstanceLocation + FVector(0, 0, 2000), InstanceLocation + FVector(0, 0, -2000), ECC_WorldStatic, CollisionParams);
+			
+			if (!bHit || HitResult.Component != TerrainMesh)
+			{
+				continue;
+			}
+
+			// Check ground slope
+			float DotProduct = FVector::DotProduct(HitResult.ImpactNormal, FVector::UpVector);
+			float SlopeAngle = FMath::RadiansToDegrees(FMath::Acos(DotProduct));
+
+			if (SlopeAngle < FoliageType->GroundSlopeAngle.Min || SlopeAngle > FoliageType->GroundSlopeAngle.Max)
+			{
+				continue;
+			}
+
+			FTransform InstanceTransform = FTransform();
+			InstanceTransform.SetLocation(HitResult.Location + FVector(0, 0, FoliageSeed.FRandRange(FoliageType->ZOffset.Min, FoliageType->ZOffset.Max)));
+			InstanceTransform.SetScale3D(FVector::One() * FoliageSeed.FRandRange(FoliageType->ProceduralScale.Min, FoliageType->ProceduralScale.Max));
+			if (FoliageType->RandomYaw)
+			{
+				InstanceTransform.SetRotation(FRotator(0, FoliageSeed.FRandRange(0, 360), 0).Quaternion());
+			}
+
+			FoliageIsmComponent->AddInstance(InstanceTransform, true);
+			InstancesPerStep++;
+		}
+		if (InstancesPerStep == 0)
+		{
+			return;
+		}
+	}
 }
 
 //void AWorldGenerator::Tick(float DeltaTime)
@@ -167,3 +378,7 @@ void AWorldGenerator::GetNormalsTangents(TArray<FVector> InVertices, TArray<int>
 //
 //}
 
+void FAsyncWorldGenerator::DoWork()
+{
+	WorldGenerator->GenerateTerrain(WorldGenerator->GetSectionIndexX(), WorldGenerator->GetSectionIndexY());
+}
--- Source/TerrainProcGen/Public/WorldGenerator.h
+++ Source/TerrainProcGen/Public/WorldGenerator.h
@@ -4,10 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "ProceduralMeshComponent.h"
 #include "WorldGenerator.generated.h"
 
 class UProceduralMeshComponent;
-struct FProcMeshTangent;
+class UFoliageType_InstancedStaticMesh;
+
+USTRUCT(BlueprintType)
+struct FFoliageInstanceData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite)
+	TArray<int> Instances;
+};
 
 UCLASS()
 class TERRAINPROCGEN_API AWorldGenerator : public AActor
@@ -18,11 +28,20 @@ public:
 	AWorldGenerator();
 	//virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION(BlueprintCallable)
+	void GenerateTerrain(const int InSectionIndexX, const int InSectionIndexY);
+
+	UFUNCTION(BlueprintCallable)
+	int DrawTile();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	bool RemoveFoliageTile(const int TileIndex);
+
 protected:
 	virtual void BeginPlay() override;
 
 	UFUNCTION(BlueprintCallable)
-	void GenerateTerrain(const int InSectionIndexX, const int InSectionIndexY);
+	void GenerateTerrainAsync(const int InSectionIndexX, const int InSectionIndexY);
 
 	UFUNCTION(BlueprintCallable)
 	int GenHeight(FVector2D Location);
@@ -33,12 +52,31 @@ protected:
 	/* For removing the seam between meshes */
 	void GetNormalsTangents(TArray<FVector> InVertices, TArray<int> InTriangles, TArray<FVector2D> InUVs, TArray<FVector>& OutNormals, TArray<FProcMeshTangent>& OutTangents, TArray<FVector>& OutVertices, TArray<int>& OutTriangles, TArray<FVector2D>& OutUVs);
 
-	//USceneComponent* SceneComponent1;
+	UFUNCTION(BlueprintCallable)
+	FVector GetPlayerLocation();
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UFUNCTION(BlueprintCallable)
+	FVector2D GetTileLocation(FIntPoint TileCoordinate);
+
+	UFUNCTION(BlueprintCallable)
+	FIntPoint GetClosestQueuedTile();
+
+	UFUNCTION(BlueprintCallable)
+	int GetFurthestUpdatableTileIndex();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void InitializeFoliageTypes();
+
+	UFUNCTION(BlueprintCallable)
+	void AddFoliageInstances(const FVector InLocation);
+
+	UFUNCTION(BlueprintCallable)
+	void SpawnFoliageCluster(UFoliageType_InstancedStaticMesh* FoliageType, UInstancedStaticMeshComponent* FoliageIsmComponent, const FVector ClusterLocation);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Procedural Generation|Defaults")
 	UProceduralMeshComponent* TerrainMesh;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Defaults")
 	UMaterialInterface* TerrainMaterial = nullptr;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Procedural Generation|Defaults")
@@ -61,13 +99,13 @@ protected:
 
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Height Params")
-	float ScaleLarge = 0.00001f; //0.00005
+	float ScaleLarge = 10000000.f; //0.00001f
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Height Params")
-	float ScaleMedium = 0.0001f; //0.0005
+	float ScaleMedium = 1000000; //0.0001f
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Height Params")
-	float ScaleSmall = 0.001f; // 0.008
+	float ScaleSmall = 100000; //0.001f
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Height Params")
 	float AmplitudeLarge = 20000.f; // 10000
@@ -78,4 +116,96 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Height Params")
 	float AmplitudeSmall = 500.f;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Defaults")
+	bool bRandomizeLayout = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Defaults")
+	FVector2D PerlinOffset;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Defaults")
+	TMap<FIntPoint, int> QueuedTiles;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Defaults")
+	float TileDiscardDistance;
+
+	int SectionIndexX = 0;
+	int SectionIndexY = 0;
+
+	TArray<int> Triangles;
+	TArray<FVector> SubNormals;
+	TArray<FProcMeshTangent> SubTangents;
+	TArray<FVector2D> SubUVs;
+	TArray<int> SubTriangles;
+	TArray<FVector> SubVertices;
+
+	UPROPERTY(BlueprintReadWrite)
+	bool bGeneratorBusy = false;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bTileDataReady = false;
+
+	/* Foliage variables */
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	TArray<UFoliageType_InstancedStaticMesh*> FoliageTypes;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	bool RandomizeFoliage = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	int InitialFoliageSeed = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	FRandomStream FoliageSeed;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	TArray<FVector> FoliagePoints;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	int GrowthProbabilityPercentage = 30;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	int MaxClusterSize = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	int InstanceOffset = 2000;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	int InstanceOffsetVariation = 200;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	FVector InstanceScale = FVector(1.f);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	float ScaleVariationMultiplier = .2f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	int FoliageSpawnPercentage = 100;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	TArray<UInstancedStaticMeshComponent*> FoliageComponents;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation|Foliage")
+	TMap<UInstancedStaticMeshComponent*, FFoliageInstanceData> ReplaceableFoliagePool;
+
+public:
+	FORCEINLINE int GetSectionIndexX() const { return SectionIndexX; }
+	FORCEINLINE int GetSectionIndexY() const { return SectionIndexY; }
+
+};
+
+class FAsyncWorldGenerator : public FNonAbandonableTask
+{
+public:
+	FAsyncWorldGenerator(AWorldGenerator* InWorldGenerator) : WorldGenerator(InWorldGenerator) {}
+
+	FORCEINLINE TStatId GetStatId() const
+	{
+		RETURN_QUICK_DECLARE_CYCLE_STAT(FAsyncWorldGenerator, STATGROUP_ThreadPoolAsyncTasks);
+	}
+
+	void DoWork();
+
+private:
+	AWorldGenerator* WorldGenerator;
 };
